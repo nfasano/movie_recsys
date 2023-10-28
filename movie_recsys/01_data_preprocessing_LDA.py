7f7f9f84b1efe8afe5d40c6bc0fe7e1dcc4858e7
Nicholas Fasano <nmfasano5@gmail.com>
2023-10-28 11:27:18 -0400

    improved documentation, renamed X to bag_of_words

--- movie_recsys/01_data_preprocessing_LDA.py
+++ movie_recsys/01_data_preprocessing_LDA.py
@@ -1,27 +1,42 @@
 """
-Preprocess film script dataset for LDA model
+01_data_preprocessing_LDA.py: Run this python file to create bag of words of film script dataset 
+which will be fed into LDA model. Options: remove stopwords, lemmatization/stemming, word mapping
 
-Description: This notebook reads in the cleaned scripts dataset and returns a bag-of-words representation, X, that will be used for LDA model training. 
+External Dependencies:
+    springfield_movie_scripts.csv: Read in as a Pd.DataFrame. Must contain the column 'script_text'
+    word_map.txt: Read in as a dictionary, contains key:value pairs for word mappings
 
-The script data is first removed of stop words and, optionally, lemmatization is performed. The corpus is then represented using a bag-of-words model. 
+returns:
+    bag_of_words (scipy sparse matrix): saved to data_preprocessing_out\\bag_of_words.npz
+    word_text (list of final vocabulary): saved to data_preprocessing_out\\word_text.txt
+
 """
 
 import pickle
 import json
-from scipy import sparse
+
 import numpy as np
 import pandas as pd
+from scipy import sparse
+
+from sklearn.feature_extraction.text import CountVectorizer
+
 import nltk
 from nltk.corpus import wordnet, stopwords
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from collections import defaultdict
-from sklearn.feature_extraction.text import CountVectorizer
 
 
 def get_wordnet_pos(word):
     """
-    input: word, a str
-    ouput: part of speech for that word, used in lemmatization function
+    Function to get the part of speech of a word. The part of speech
+    will eventually be used into the lemmatization method.
+
+    Args:
+        word (str): a single string
+
+    Returns:
+        pos (str): part of speech of word
     """
     tag = nltk.pos_tag([word])[0][1][0].upper()
     tag_dict = {
@@ -30,22 +45,35 @@ def get_wordnet_pos(word):
         "V": wordnet.VERB,
         "R": wordnet.ADV,
     }
-    return tag_dict.get(tag, wordnet.NOUN)
+
+    pos = tag_dict.get(tag, wordnet.NOUN)
+    return pos
 
 
 def lemma(text):
     """
-    input: text, a str
-    ouput: lemmatized text, a str
+    Function to transform all words in a string to their lemmatized form.
+    e.g. "I am caring" -> "I be care"
+
+    Args:
+        text (str)
+    Returns:
+        lemmatized text (str)
     """
+
     lemmatizer = WordNetLemmatizer()
     return " ".join([lemmatizer.lemmatize(w, get_wordnet_pos(w)) for w in text.split()])
 
 
 def stem(text):
     """
-    input: text, a str
-    ouput: stemmed text, a str
+    Function to transform all words in a string to their stemmed form.
+    e.g. "I am caring" -> "I am car"
+
+    Args:
+        text (str)
+    Returns:
+        stemmed text (str)
     """
     ps = PorterStemmer()
     return " ".join([ps.stem(w) for w in text.split()])
@@ -53,14 +81,14 @@ def stem(text):
 
 def list_duplicates(word_list):
     """
-    description: after lemmatization is performed, find all words
-                 in a list that are duplicated and return the duplicates
-                 as a dictionary with values of the index where the duplicated word occurs
+    Function to be called after lemmatization is performed. It finds all words
+    in a list that are duplicated and returns the duplicates
 
-    input:
-            - word_list, a list of lemmatized words
-    output:
-            - a tuple of (key, locs) where key is the lemmatized word
+    Args:
+            - word_list (list): a list of lemmatized words
+
+    Returns:
+            - (key, locs): a list of tuples where the key is the lemmatized word
               and locs are the indice locations of duplicated entries
     """
     tally = defaultdict(list)
@@ -71,14 +99,16 @@ def list_duplicates(word_list):
 
 def df_preprocessing(df, word_map, stop_words):
     """
-    description: function for preprocesing the entire corpus
-                 methods include lowercase, punctuation removal, word mappings, and removing stop words
-    input:
-            - df, a pd.DataFrame with columns ['script_text']
-            - word_map, a dictionary for mapping one word to another (e.g. ok maps to okay)
-            - stop_words, a list of strings to be removed from corpus
-    output:
-            - df_processed, a pd.DataFrame
+    Function for preprocesing the entire corpus methods include lowercase,
+    punctuation removal, word mappings, and removing stop words
+
+    Args:
+        df (pd.DataFrame): DataFrame with columns ['script_text']
+        word_map (dictionary): key-value pairs for mapping one word to another (e.g. ok:okay)
+        stop_words (list of str): List of words to be removed from corpus
+
+    Returns:
+        df_processed (pd.DataFrame)
     """
 
     # remove punctuation
@@ -105,27 +135,29 @@ def df_preprocessing(df, word_map, stop_words):
 
 def construct_BoW(df, n_features=10000, max_df=0.8, min_df=1, lemmatization=False):
     """
-    inputs:
-            - df, a pd.DataFrame with columns ['script_text'] that will be used for Bag of words (BoW) construction
-            - n_features, an int, maximum number of columns in BoW matrx, X
-            - max_df, max word counts (if int) or doc frequency (if float between 0 and 1)
-            - min_df, min word counts (if int) or doc frequency (if float between 0 and 1)
-            - lemmatization, a bool, whether or not to apply lemmatization (default is False)
-    ouputs:
-            - X_out, a sparse matrix of size (num_documents x n_features)
-            - word_key_out, a list of strings for the features used to create X_out, sorted by increasing columns of X_out
+    Function to crete a bag of words for script text
+
+    Args:
+            - df (pd.DataFrame): DataFrame with columns ['script_text']
+            - n_features (int): maximum number of words for vocabulary
+            - max_df (int or float): max word counts (if int) or max doc frequency (if float between 0 and 1)
+            - min_df (int or float): min word counts (if int) or min doc frequency (if float between 0 and 1)
+            - lemmatization (bool): whether or not to apply lemmatization (default is False)
+    Returns:
+            - bag_of_words (scipy sparse matrix): a sparse matrix of size (num_documents x n_features)
+            - word_key (list of str): features used to create bag_of_words
     """
     if not lemmatization:
         # use scikit-learn's CountVectorizer to create BoW representation
         vectorizer = CountVectorizer(
             max_df=max_df, min_df=min_df, max_features=n_features, stop_words="english"
         )
-        X_out = vectorizer.fit_transform(df["script_text"])
+        bag_of_words = vectorizer.fit_transform(df["script_text"])
 
         # get word features in increasing item number
-        word_key_out = vectorizer.get_feature_names_out()
+        word_key = vectorizer.get_feature_names_out()
 
-        return X_out, word_key_out
+        return bag_of_words, word_key
     else:
         # first create bag of words on corpus then perform lemmatization
         vectorizer = CountVectorizer(max_df=0.7, min_df=5)
@@ -156,13 +188,13 @@ def construct_BoW(df, n_features=10000, max_df=0.8, min_df=1, lemmatization=Fals
             X_lil[:, dup[1][0]] = np.sum(X_csc[:, dup[1][:]], axis=1)
 
         # construt output matrix and drop the duplicated columns, update word_key
-        X_out = X_lil.tocsr()
+        bag_of_words = X_lil.tocsr()
         words_to_keep = np.delete(word_idx, words_to_remove)
-        X_out = X_out[:, words_to_keep]
+        bag_of_words = bag_of_words[:, words_to_keep]
         word_key = [word_key[jword] for jword in words_to_keep]
 
         # keep words that only appear in more than df_min documents but less than df_max documents
-        X_bin = X_out.copy()
+        X_bin = bag_of_words.copy()
         X_bin.data = np.ones(shape=X_bin.data.shape)
 
         min_df = min_df * len(df) if type(min_df) == float else min_df
@@ -172,29 +204,29 @@ def construct_BoW(df, n_features=10000, max_df=0.8, min_df=1, lemmatization=Fals
             np.argwhere(np.sum(X_bin, axis=0) > min_df)[:, 1],
             np.argwhere(np.sum(X_bin, axis=0) < max_df)[:, 1],
         )
-        X_out = X_out[:, word_idx]
+        bag_of_words = bag_of_words[:, word_idx]
         word_key = [word_key[jword] for jword in word_idx]
 
         # # drop rows that have no words
-        # non_blank_movie_scripts = np.argwhere(np.sum(Xbin, axis=1) > 0)[:, 0]
-        # X_out = X_out[non_blank_movie_scripts, :]
+        # non_blank_movie_scripts = np.argwhere(np.sum(X_bin, axis=1) > 0)[:, 0]
+        # bag_of_words = bag_of_words[non_blank_movie_scripts, :]
 
         # finally retain only top n_features, update word_key accordingly
         wordcount_ordered = np.flip(
-            np.argsort(np.array(np.sum(X_out, axis=0)).reshape(-1))
+            np.argsort(np.array(np.sum(bag_of_words, axis=0)).reshape(-1))
         )
-        X_out = X_out[:, wordcount_ordered[0:n_features]]
+        bag_of_words = bag_of_words[:, wordcount_ordered[0:n_features]]
         word_key = [word_key[jword] for jword in wordcount_ordered[0:n_features]]
 
         # return lemmatized word_key
-        word_key_out = lemma(" ".join([wk for wk in word_key])).split()
+        word_key = lemma(" ".join([wk for wk in word_key])).split()
 
-        return X_out, word_key_out
+        return bag_of_words, word_key
 
 
 if __name__ == "__main__":
     # import cleaned text data from data_cleaning_and_synthesis.ipynb notebook
-    path_to_csv = "..\\database\\dataset_film_scripts\\springfield_movie_scripts_2023_01_13_clean.csv"
+    path_to_csv = "..\\database\\dataset_film_scripts\\springfield_movie_scripts.csv"
 
     print(f"Loading in cleaned movie script dataset...")
     df = pd.read_csv(path_to_csv, index_col=[0])
@@ -218,13 +250,13 @@ if __name__ == "__main__":
     print("Done!")
 
     print(f"Create bag of words representation...")
-    X, word_key = construct_BoW(
+    bag_of_words, word_key = construct_BoW(
         df, n_features=10000, max_df=0.8, min_df=3, lemmatization=False
     )
     print("Done!")
 
     # save bag-of-words matrix and corresponding word_key
-    sparse.save_npz("data_preprocessing_out\\X.npz", X)
+    sparse.save_npz("data_preprocessing_out\\bag_of_words.npz", bag_of_words)
 
     with open("data_preprocessing_out\\word_key.txt", "wb") as f:
         pickle.dump(word_key, f)
